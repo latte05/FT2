f791e72d3e9d893da315430c2ab71a10276e49ad
rails course <camaroa@hotmail.com>
2017-03-14 06:48:05 +0000

    added many-to-many association

---
+++ db/migrate/20170312050132_create_user_stocks.rb
@@ -0,0 +1,11 @@
+class CreateUserStocks < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :user_stocks
+    create_table :user_stocks do |t|
+      t.integer :user_id
+      t.integer :stock_id
+
+      t.timestamps
+    end
+  end
+end
